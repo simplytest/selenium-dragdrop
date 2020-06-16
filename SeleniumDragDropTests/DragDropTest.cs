186114e17596079457567089c062d3da725894f9
Alexander Heimlich <34610354+axheim@users.noreply.github.com>
2020-06-16 10:10:35 +0200

    unit tests updated

--- SeleniumDragDropTests/DragDropTest.cs
+++ SeleniumDragDropTests/DragDropTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 
 namespace SeleniumDragDropTest
@@ -8,13 +9,15 @@ namespace SeleniumDragDropTest
     [TestClass]
     public class DragDropTest
     {
+        /// This new style HTML 5 Drag & Drop implementation does not work with native Selenium Actions, but DragDropHelper version works.
         [TestMethod]
-        public void TestMethod1()
+        public void TestDragDrop()
         {
-            
-            IWebDriver driver = new ChromeDriver(Environment.CurrentDirectory);
+
+            IWebDriver driver = null;
             try
             {
+                driver = new ChromeDriver(Environment.CurrentDirectory);
                 driver.Navigate().GoToUrl("https://www.w3schools.com/html/html5_draganddrop.asp");
 
                 IWebElement sourceEl = driver.FindElement(By.Id("drag1"));
@@ -27,8 +30,43 @@ namespace SeleniumDragDropTest
             }
             finally
             {
-                driver.Quit();
+                if (driver != null)
+                    driver.Quit();
             }
         }
+
+
+        /// This old style JQuery Drag & Drop implementation works even with native Selenium Actions
+        [TestMethod]
+        public void SeleniumDragDropNonHTML5()
+        {
+
+            IWebDriver driver = null;
+            try
+            {
+                driver = new ChromeDriver(Environment.CurrentDirectory);
+                driver.Navigate().GoToUrl("http://jqueryui.com/droppable");
+                driver.SwitchTo().Frame(0);
+
+                IWebElement sourceEl = driver.FindElement(By.Id("draggable"));
+                IWebElement targetEl = driver.FindElement(By.Id("droppable"));
+
+                Actions action = new Actions(driver);
+                action
+                    .ClickAndHold(sourceEl)
+                    .MoveToElement(targetEl)
+                    .Release()
+                    .Build().Perform();
+
+                Assert.IsNotNull(targetEl.FindElement(By.XPath("//p[text() = 'Dropped!']")));
+            }
+            finally
+            {
+                if (driver != null)
+                    driver.Quit();
+            }
+        }
+
+
     }
 }
